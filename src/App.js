ca539a2ba5c4de436c420948822bd04151aaa7ee
phamvandureactjs <phamvandu.uet.vnu@gmail.com>
2023-02-16 20:37:58 +0700

    TH4

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+import React ,{useState} from 'react';
+const useModal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const showHideClassName = isOpen ? "modal d-block" : "modal d-none";
+  const modal = (
+    <div className={showHideClassName}>
+      <div className="modal-container">
+        <h2>Hello Modal</h2>
+        <div className='form-group'>
+          <label>Hello</label>
+        </div>
+        <button onClick={() => setIsOpen(!isOpen)} type = "button">close</button>
+      </div>
+    </div>
+  );
+  return {isOpen, setIsOpen, modal};
+};
+function App() {
+  const {modal, isOpen, setIsOpen} = useModal();
+  const onOpenModal = () => setIsOpen(true);
+  return (
+    <div className="container">
+      <div>Welcome</div>
+      <div onClick={onOpenModal}>Open modal</div>
+      {modal}
+    </div>
+  );
+}
+
+export default App;
